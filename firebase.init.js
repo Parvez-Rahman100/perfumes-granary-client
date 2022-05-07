17e04f72e17126658eb11dd9f4d4978a0e6f2327
Parvez-Rahman100 <iamparvezrahman@gmail.com>
2022-05-07 13:48:59 +0600

    navbar added

---
+++ firebase.init.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD7BvA8QZ1CZHAhzX5WoYLHxvFDkClI3qw",
+  authDomain: "perfumes-granary.firebaseapp.com",
+  projectId: "perfumes-granary",
+  storageBucket: "perfumes-granary.appspot.com",
+  messagingSenderId: "669265864660",
+  appId: "1:669265864660:web:eec9439ac6b2b360de5841"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export default auth;
