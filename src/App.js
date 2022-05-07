272c44e3a159fcbea94a6471fec52b32d75869aa
Parvez-Rahman100 <iamparvezrahman@gmail.com>
2022-05-07 14:16:05 +0600

    routes added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import {  Route, Routes } from 'react-router-dom';
 import './App.css';
+import Blogs from './Pages/Blogs/Blogs';
 import Header from './Pages/Home/Header/Header';
+import Home from './Pages/Home/Home/Home';
+import Products from './Pages/Products/Products';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/products' element={<Products></Products>}></Route>
+      </Routes>
     </div>
   );
 }
--- src/Pages/Home/Header/Header.js
+++ src/Pages/Home/Header/Header.js
@@ -4,19 +4,19 @@ import { Container, Nav, Navbar } from 'react-bootstrap';
 const Header = () => {
     return (
         <div>
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Navbar style={{backgroundColor: 'rgba(255, 107, 129,1.0)'}} collapseOnSelect expand="lg"  variant="dark">
   <Container>
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Brand href="#home">Perfumes Granary</Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="me-auto">
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
+      <Nav.Link href="#features">Home</Nav.Link>
+      <Nav.Link href="#pricing">Products</Nav.Link>
     </Nav>
     <Nav>
-      <Nav.Link href="#deets">More deets</Nav.Link>
-      <Nav.Link eventKey={2} href="#memes">
-        Dank memes
+      <Nav.Link href="#deets">Blogs</Nav.Link>
+      <Nav.Link href="#memes">
+        Login
       </Nav.Link>
     </Nav>
   </Navbar.Collapse>
