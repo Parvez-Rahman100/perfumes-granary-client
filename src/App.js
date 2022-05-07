1d3de9d39c1628a9534c00fda8fc41e35ddef53c
Parvez-Rahman100 <iamparvezrahman@gmail.com>
2022-05-07 22:50:32 +0600

    requireAuth added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from './Pages/Home/Header/Header';
 import Home from './Pages/Home/Home/Home';
 import Login from './Pages/Login/Login/Login';
 import Register from './Pages/Login/Register/Register';
+import RequireAuth from './Pages/Login/RequireAuth/RequireAuth';
 import Products from './Pages/Products/Products';
 import Footer from './Pages/Shared/Footer/Footer';
 import NotFound from './Pages/Shared/NotFound/NotFound';
@@ -14,12 +15,13 @@ function App() {
   return (
     <div className="App">
       <Header></Header>
-      
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/blogs' element={<Blogs></Blogs>}></Route>
-        <Route path='/products' element={<Products></Products>}></Route>
+        <Route path='/products' element={<RequireAuth>
+          <Products></Products>
+        </RequireAuth>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/register' element={<Register></Register>}></Route>
         <Route path='*' element={<NotFound></NotFound>}></Route>
--- src/Pages/Home/Header/Header.js
+++ src/Pages/Home/Header/Header.js
@@ -1,8 +1,15 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../../firebase.init';
 
 const Header = () => {
+  const [user] = useAuthState(auth);
+  const handleSignOut = () =>{
+    signOut(auth);
+  }
     return (
         <div>
             <Navbar style={{backgroundColor: 'rgba(255, 107, 129,1.0)'}} collapseOnSelect expand="lg"  variant="dark">
@@ -16,9 +23,13 @@ const Header = () => {
     </Nav>
     <Nav>
       <Nav.Link as={Link} to="/blogs">Blogs</Nav.Link>
-      <Nav.Link as={Link} to="/login">
-        Login
-      </Nav.Link>
+      { user ? 
+
+<button className='btn btn-link text-white text-decoration-none' onClick={handleSignOut}>Sign Out</button>
+:
+  <Nav.Link as={Link} to="/login">
+  Login
+</Nav.Link>}
     </Nav>
   </Navbar.Collapse>
   </Container>
